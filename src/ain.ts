9fee8d1682a5a3a55daa59cc6f77043c181abdbb
akaster99 <akaster99@comcom.ai>
2024-04-08 17:49:18 +0900

    feat: remove ainize.js

--- src/ain.ts
+++ src/ain.ts
@@ -1,7 +1,9 @@
 import Ain from "@ainblockchain/ain-js";
+import { Path, getBlockChainEndpoint } from "./constants";
+import Service from "@ainize-team/ainize-js/dist/service";
 
 export default class AinModule {
-  private ain?: Ain;
+  private ain!: Ain;
   private static instance: AinModule;
 
   static getInstance() {
@@ -11,8 +13,23 @@ export default class AinModule {
     return AinModule.instance;
   }
 
-  initAin(chainId: 0 | 1) {
+  initAin(chainId: 0 | 1, privateKey: string ) {
     const blockchainEndpoint = getBlockChainEndpoint(chainId);
     this.ain = new Ain(blockchainEndpoint, chainId);
+    this.ain.wallet.setDefaultAccount(privateKey);
+  }
+
+  async getValue(path: string) {
+    return await this.ain.db.ref(path).getValue();
+  }
+
+
+  async getService(serviceName: string): Promise<Service> {
+    const servicePath = Path.app(serviceName).root();
+    const serviceData = await this.getValue(servicePath);
+    if(!serviceData) {
+      throw new Error("Service not found");
+    }
+    return new Service(serviceName);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,51 @@
 import express, { Express, Request, Response } from 'express';
-import Ain from "@ainblockchain/ain-js";
 import dotenv from 'dotenv';
 import { RESPONSE_STATUS } from '@ainize-team/ainize-js/dist/types/type';
 import { getBlockChainEndpoint } from './constants';
+import AinModule from './ain';
+import Middleware from './middlewares/middleware';
+import { extractDataFromServiceRequest } from './utils/extractor';
 dotenv.config();
 const privateKey = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : '';
 const chainId = parseInt(process.env.BLOCKCHAIN_NETWORK ? process.env.BLOCKCHAIN_NETWORK : '1');
 const port = process.env.PORT ? process.env.PORT : '8000';
+const blockchainEndpoint = getBlockChainEndpoint(chainId);
+const ainModule = new AinModule();
+if (chainId != 0 && chainId != 1) {
+  throw new Error('Invalid chain Id.');
+}
+ainModule.initAin(chainId, privateKey);
+const middleware = new Middleware();
 const app: Express = express();
 app.use(express.json());
 
-const blockchainEndpoint = getBlockChainEndpoint(chainId);
-const ain = new Ain(blockchainEndpoint, chainId);
-ain.wallet.setDefaultAccount(privateKey);
-
 app.post('/service',
-  blockchainTriggerFilter,
+  middleware.blockchainTriggerFilter,
   async (req: Request, res: Response) => {
-  const { appName, requestData, requestKey } = ainize.internal.getDataFromServiceRequest(req);
+  const { appName, requestData, requestKey } = extractDataFromServiceRequest(req);
   console.log("service requestKey: ", requestKey);
   try{
-    const service = await ainize.getService(appName);
+    const service = await ainModule.getService(appName);
     const amount = 0.1;
     console.log(appName, requestData, amount);
-    await ainize.internal.handleRequest(req, amount, RESPONSE_STATUS.SUCCESS, responseData);
+    // TODO: replace handleRequest
+    // await ainize.internal.handleRequest(req, amount, RESPONSE_STATUS.SUCCESS, responseData);
   }catch(e) {
-    await ainize.internal.handleRequest(req, 0, RESPONSE_STATUS.FAIL,'error');
+    // TODO: replace handleRequest
+    // await ainize.internal.handleRequest(req, 0, RESPONSE_STATUS.FAIL,'error');
     console.log('error: ',e);
     res.send('error');
   }
 });
 
 app.post('/deposit',
-  ainize.middleware.triggerDuplicateFilter,
+  middleware.blockchainTriggerFilter,
   async (req: Request, res:Response) => {
   console.log("deposit");
   try{ 
-    const result = await ainize.internal.handleDeposit(req);
-    console.log(result);
+    // TODO: replace handleDeposit
+    // const result = await ainize.internal.handleDeposit(req);
+    // console.log(result);
   }catch(e) {
     console.log('error: ',e);
     res.send('error');
