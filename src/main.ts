13e80305cf68f2a5f4db43b6af49d32ad0891b26
akaster99 <akaster99@comcom.ai>
2024-04-08 13:41:58 +0900

    initial setting

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import Ainize from '@ainize-team/ainize-js';
+import { RESPONSE_STATUS } from '@ainize-team/ainize-js/dist/types/type';
+dotenv.config();
+const userPrivateKey = process.env.PRIVATE_KEY? process.env.PRIVATE_KEY : '';
+const app: Express = express();
+app.use(express.json());
+const port = process.env.PORT;
+const ainize = new Ainize(0);
+ainize.login(userPrivateKey);
+
+app.post('/service',
+  ainize.middleware.blockchainTriggerFilter,
+  async (req: Request, res: Response) => {
+  const { appName, requestData, requestKey } = ainize.internal.getDataFromServiceRequest(req);
+  console.log("service requestKey: ", requestKey);
+  try{
+    const service = await ainize.getService(appName);
+    const amount = 0.1;
+    console.log(appName, requestData, amount);
+    await ainize.internal.handleRequest(req, amount, RESPONSE_STATUS.SUCCESS, responseData);
+  }catch(e) {
+    await ainize.internal.handleRequest(req, 0, RESPONSE_STATUS.FAIL,'error');
+    console.log('error: ',e);
+    res.send('error');
+  }
+});
+
+app.post('/deposit',
+  ainize.middleware.triggerDuplicateFilter,
+  async (req: Request, res:Response) => {
+  console.log("deposit");
+  try{ 
+    const result = await ainize.internal.handleDeposit(req);
+    console.log(result);
+  }catch(e) {
+    console.log('error: ',e);
+    res.send('error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
