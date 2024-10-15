03a43fb7f105fccf6bc52b81ea905c3df8467aef
akaster99 <idavid29@naver.com>
2024-10-15 15:26:46 +0900

    feat: add api key

--- src/inference.ts
+++ src/inference.ts
@@ -1,10 +1,28 @@
-import { Ollama } from 'ollama'
 export const inference = async (prompt:string):Promise<string> =>{
-  const OLLAMA_URL = process.env.OLLAMA_URL
-  const ollama = new Ollama({ host: OLLAMA_URL })
-  const response = await ollama.generate({
-    model: 'gemma2:27b-instruct-fp16',
-    prompt: prompt,
-  })
-return response.response
+  const response = await fetch(
+    String(process.env.INFERENCE_URL),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${String(process.env.API_KEY)}`
+      },
+      body: JSON.stringify({
+        model: String(process.env.MODEL_NAME),
+        mesage: {
+          role: 'user',
+          content: {
+            type: 'text',
+            text: prompt
+          }
+        },
+        stream: false
+      })
+    }
+  )
+  if (!response.ok) {
+    throw new Error(`Fail to inference: ${response.statusText}`);
+  }
+  const data = await response.json();
+  return data.choices[0].message.content;
 }
